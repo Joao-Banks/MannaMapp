68d306f044958222137209e8f2aa501f60188c33
febarnett3 <113536068+febarnett3@users.noreply.github.com>
2025-09-25 13:41:07 -0600

    hero and search section created

---
+++ src/components/Hero.js
@@ -0,0 +1,4 @@
+function Hero() {
+  return <p>this is the hero section.</p>;
+}
+export default Hero;
---
+++ src/components/Search.js
@@ -0,0 +1,4 @@
+function Search() {
+  return <p>this is the search section</p>;
+}
+export default Search;
