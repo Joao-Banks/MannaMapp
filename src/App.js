4d529c916eca6cf7acdc097d860fcca810ebae2c
Sydney Ahlstrom <ahl19001@byui.edu>
2025-11-26 19:11:05 -0700

    hoorayyyyy

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Search from "./components/Search";
 import Footer from "./components/Footer";
+import breadBackground from "./assets/breadbackground.webp";
 
 function App() {
   return (
-    <div>
-      <h1 className="title">Manna Map</h1>
+    <div className="app-wrapper">
+      {/* Left and Right fixed side panels */}
+      <div className="side-panel left" style={{ backgroundImage: `url(${breadBackground})` }}>
+      </div>
+      <div className="side-panel right" style={{ backgroundImage: `url(${breadBackground})` }}>
+      </div>
+      <div className="main-content">
+        <h1 className="title">Manna Map</h1>
 
-      <p className="tagline">"Helping you find spiritual nourishment near you."</p>
+        <p className="tagline">"Helping you find spiritual nourishment near you."</p>
 
-      <Hero />
-      <Search />
-      <Footer></Footer>
+        <Hero />
+        <Search />
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
