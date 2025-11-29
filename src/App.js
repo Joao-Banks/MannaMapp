ab14ea7bba8309b3e1afcd17fc011bf7a6030121
febarnett3 <113536068+febarnett3@users.noreply.github.com>
2025-11-28 19:05:56 -0700

    bread side panels fixed

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Search from "./components/Search";
 import Footer from "./components/Footer";
-import breadBackground from "./assets/breadbackground.webp";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      {/* Left and Right fixed side panels */}
-      <div className="side-panel left" style={{ backgroundImage: `url(${breadBackground})` }}>
-      </div>
-      <div className="side-panel right" style={{ backgroundImage: `url(${breadBackground})` }}>
-      </div>
-      <div className="main-content">
-        <h1 className="title">Manna Map</h1>
+    <div className="main-content">
+      <h1 className="title">Manna Map</h1>
 
-        <p className="tagline">"Helping you find spiritual nourishment near you."</p>
+      <p className="tagline">"Helping find spiritual nourishment near you."</p>
 
-        <Hero />
-        <Search />
-        <Footer></Footer>
-      </div>
+      <Hero />
+      <Search />
+      <Footer />
     </div>
   );
 }
